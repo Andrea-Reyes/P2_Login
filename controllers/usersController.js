9cc4a8a1541562ba70c7e5016e37acb24dfd9da9
unknown <karen.reyeslem@gmail.com>
2025-09-08 00:37:35 -0600

    Actualizacion del archivo userController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -30,29 +30,8 @@ function create (req,res){
     });
 }
 
-//Metodo para loguear a un usuario
-function login(req, res) {
-    const { email, password } = req.body;
-
-    //Respuesta de estatus 400
-    if (!email || !password) {
-        return res.status(400).json({ Error: "Por favor ingrese usario y contraseña para iniciar sesión." });
-    } 
-
-    //Determinar si el usuario existe
-    const usuario = users.find(u => u.email === email && u.password === password);
-    if (!usuario) {
-        return res.status(404).json({ Error: "Usuario o contraseña inconrrectos" });
-    }
-
-    //Respuesta de estatus 200
-    res.status(200).json({
-        Status: `¡Bienvenid@ ${usuario.name}, has iniciado sesión!`
-    });
-}
-
+//Exportar métodos
 export {
     getAll, 
-    create,
-    login
+    create
 };
