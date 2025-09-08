4a935264e4a92c47e1ad7f6a6512ec394f1d258a
unknown <karen.reyeslem@gmail.com>
2025-09-07 20:07:24 -0600

    Actualizacion de databases

---
+++ databases/tokensDatabase.js
@@ -0,0 +1,16 @@
+const tokens = [
+  {
+    "usuario": "karen.reyes@correo.com",
+    "token": "b0a7d3c4-9f62-42a5-8db7-7a51c1f49e33",
+    "creacion": "2025-09-08T02:06:02.000Z",
+    "status": "Active"
+  },
+  {
+    "usuario": "andrea.lem@correo.com",
+    "token": "6d41e3a7-2d8f-49b5-a0e0-2cf8a69d5a1b",
+    "creacion": "2025-09-08T02:04:30.707Z",
+    "status": "Active"
+  }
+]
+
+export default tokens;
---
+++ databases/usersDatabase.js
@@ -0,0 +1,16 @@
+const users = [
+  {
+    "id": "1c3d7f9a-0c2e-4a65-9df4-7f2b1c8e73a0",
+    "name": "Andrea Reyes",
+    "correo": "andrea.reyes@correo.com",
+    "password": "passwordDefault"
+  },
+  {
+    "id": "f7e9b2c4-64e2-4e15-b53d-54ad46d2eae7",
+    "name": "Andrea Lem",
+    "email": "andrea.lem@correo.com",
+    "password": "passwordDefault"
+  }
+]
+
+export default users;
