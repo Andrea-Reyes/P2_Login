9a6e17d7b73d4a075d88cf3dcd5a897645733eea
unknown <karen.reyeslem@gmail.com>
2025-09-07 21:28:27 -0600

    Implementacion funcion login

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -19,7 +19,7 @@ function create (req,res){
     }
 
     //Respuesta de estatus 400
-    if (!body.email || !body.password) {
+    if (!body.name || !body.email || !body.password) {
         return res.status(400).json({ Error: "Por favor complete todos los campos para registrarse." });
     } 
     
@@ -30,7 +30,29 @@ function create (req,res){
     });
 }
 
+//Metodo para loguear a un usuario
+function login(req, res) {
+    const { email, password } = req.body;
+
+    //Respuesta de estatus 400
+    if (!email || !password) {
+        return res.status(400).json({ Error: "Por favor ingrese usario y contraseña para iniciar sesión." });
+    } 
+
+    //Determinar si el usuario existe
+    const usuario = users.find(u => u.email === email && u.password === password);
+    if (!usuario) {
+        return res.status(404).json({ Error: "Usuario o contraseña inconrrectos" });
+    }
+
+    //Respuesta de estatus 200
+    res.status(200).json({
+        Status: `¡Bienvenid@ ${usuario.name}, has iniciado sesión!`
+    });
+}
+
 export {
     getAll, 
-    create
+    create,
+    login
 };
