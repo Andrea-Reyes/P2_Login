a49a7130f44f05fd25170cfafc7feda62a827f1c
unknown <karen.reyeslem@gmail.com>
2025-09-08 00:44:37 -0600

    Implementaciond de ruta login

--- routes.js
+++ routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import * as usersController from "./controllers/usersController.js";
+import * as authController from "./controllers/authController.js";
 
 const router = express.Router();
 
 //Creacion de paths disponibles
 router.get("/getAll", usersController.getAll);
 router.post("/registro", usersController.create);
-router.get("/login", usersController.login);
+router.get("/login", authController.login);
 
 //Exportacion de este archivo
 export default router;
