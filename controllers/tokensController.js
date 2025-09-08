39ee4424261a6b12806d03b508b80456f3dd8b6f
unknown <karen.reyeslem@gmail.com>
2025-09-08 00:43:16 -0600

    Implementacion de funcion generarToken

---
+++ controllers/tokensController.js
@@ -0,0 +1,27 @@
+import { v4 as uuidv4 } from "uuid";
+import users from "../databases/usersDatabase.js";
+import tokens from "../databases/tokensDatabase.js";
+
+//Método para generar un token y url
+function generarToken(usuario){
+    const nuevoToken = {
+        usuario: usuario,
+        token: uuidv4(),
+        creacion: new Date(),
+        expira: Math.floor(Date.now()/1000)+(60*60),
+        status: "Active"
+    }
+
+    console.log(nuevoToken);
+    tokens.push(nuevoToken);
+    return {
+        URL: `https://localhost:3000/resetPassword?token=${nuevoToken.token}`,
+        Token: nuevoToken.token,
+        Expira: new Date(nuevoToken.expira * 1000).toLocaleString()
+    };
+}
+
+//Exportar métodos
+export {
+    generarToken
+};
