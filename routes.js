d94b9db881bbe542849ffc1b5a5e481939843f60
unknown <karen.reyeslem@gmail.com>
2025-09-08 00:45:13 -0600

    Implementaciond de ruta recuperacionContrasena

--- routes.js
+++ routes.js
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get("/getAll", usersController.getAll);
 router.post("/registro", usersController.create);
 router.get("/login", authController.login);
+router.get("/recuperarContrasena", authController.forgotPass);
 
 //Exportacion de este archivo
 export default router;
